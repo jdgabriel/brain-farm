000d9c2506ade4e6c57100464ea0beba30ebe151
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2025-06-13 12:41:29 -0300

    chore: update brain farm module

--- apps/server/src/application/brain-farm.module.ts
+++ apps/server/src/application/brain-farm.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { FarmModule } from './farm/farm.module';
 import { ProducerModule } from './producer/producer.module';
 
 @Module({
-  imports: [ProducerModule],
+  imports: [ProducerModule, FarmModule],
 })
 export class BrainFarmModule {}
